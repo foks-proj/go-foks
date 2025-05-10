ae1911e942e73cd5f5c8fe85c12fb06c3d1fd553
Maxwell Krohn <themax@gmail.com>
2025-05-10 07:38:04 -0400

    fixup test

--- integration-tests/cli/nag_test.go
+++ integration-tests/cli/nag_test.go
@@ -20,36 +20,48 @@ func TestNagAndClear(t *testing.T) {
 	newUserWithAgentAtVHost(t, x, 0)
 	merklePoke(t)
 
-	var nag bool
+	var nag lcl.UnifiedNagRes
+
+	assertDeviceNag := func(nag lcl.UnifiedNagRes) {
+		require.Equal(t, 1, len(nag.Nags))
+		typ, err := nag.Nags[0].GetT()
+		require.NoError(t, err)
+		require.Equal(t, lcl.NagType_TooFewDevices, typ)
+		donag := nag.Nags[0].Toofewdevices().DoNag
+		require.True(t, donag)
+	}
+
+	assertNoNag := func(nag lcl.UnifiedNagRes) {
+		require.Equal(t, 0, len(nag.Nags))
+	}
 
 	x.runCmdToJSON(t, &nag, "test", "get-device-nag")
-	require.True(t, nag)
+	assertDeviceNag(nag)
 
 	// Should be no rate limit in test
 	x.runCmdToJSON(t, &nag, "test", "get-device-nag")
-	require.True(t, nag)
+	assertDeviceNag(nag)
 
 	// With rate limit, it shouldn't show...
 	x.runCmdToJSON(t, &nag, "test", "get-device-nag", "--rate-limit")
-	require.False(t, nag)
+	assertNoNag(nag)
 
 	x.runCmdToJSON(t, &nag, "test", "get-device-nag")
-	require.True(t, nag)
+	assertDeviceNag(nag)
 
 	x.runCmd(t, nil, "notify", "clear-device-nag")
 
 	x.runCmdToJSON(t, &nag, "test", "get-device-nag")
-	require.False(t, nag)
+	assertNoNag(nag)
 
 	x.runCmd(t, nil, "notify", "clear-device-nag", "--reset")
 	x.runCmdToJSON(t, &nag, "test", "get-device-nag")
-	require.True(t, nag)
+	assertDeviceNag(nag)
 
 	var res lcl.BackupHESP
 	x.runCmdToJSON(t, &res, "backup", "new")
 
 	// Once we add a new device, no more nag
 	x.runCmdToJSON(t, &nag, "test", "get-device-nag")
-	require.False(t, nag)
-
+	assertNoNag(nag)
 }
