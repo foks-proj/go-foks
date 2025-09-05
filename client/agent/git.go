65eb1233f8c3026697415ee5b4dddff7ef0fab0e
Maxwell Krohn <themax@gmail.com>
2025-09-05 18:10:24 -0400

    cmd: `git ls --all-teams` new feature (#206)
    - working and lightly tested
    - close #197
    - to be released in v0.1.3

--- client/agent/git.go
+++ client/agent/git.go
@@ -143,61 +143,105 @@ func (c *AgentConn) GitCreate(
 	return url, nil
 }
 
-func (c *AgentConn) GitLs(ctx context.Context, cfg lcl.KVConfig) ([]proto.GitURL, error) {
-	m, tm, err := c.kvInit(ctx, cfg)
+func (c *AgentConn) GitLs(ctx context.Context, arg lcl.GitLsArg) ([]proto.GitURL, error) {
+	m, tm, err := c.kvInit(ctx, arg.Cfg)
 	if err != nil {
 		return nil, err
 	}
 	path := proto.KVPathAbs(libgit.GitPathPrefix...)
-	var did *proto.DirID
-	var ret []proto.GitURL
-	var strt proto.KVListPagination
-	var eof bool
 
-	fqpp, err := libclient.ActivePartyNamed(m.MetaContext, cfg.ActingAs)
+	listForParty := func(p *proto.FQTeamParsed) ([]proto.GitURL, error) {
+		cfg := arg.Cfg
+
+		var did *proto.DirID
+		var ret []proto.GitURL
+		var strt proto.KVListPagination
+		var eof bool
+
+		fqpp, err := libclient.ActivePartyNamed(m.MetaContext, p)
+		if err != nil {
+			return nil, err
+		}
+		cfg.ActingAs = p
+
+		listSome := func() error {
+			opts := rem.KVListOpts{
+				Num:   128,
+				Start: strt,
+			}
+
+			ls, err := tm.List(m, cfg, path, did, opts)
+			if err != nil {
+				return err
+			}
+			for _, ent := range ls.Ents {
+				if !ent.Value.IsDir() {
+					continue
+				}
+				ret = append(ret, proto.GitURL{
+					Proto: proto.GitProtoType_Foks,
+					Fqp:   *fqpp,
+					Repo:  proto.GitRepo(ent.Name),
+				})
+			}
+			if ls.Nxt != nil {
+				did = &ls.Nxt.Id
+				strt = ls.Nxt.Nxt
+			} else {
+				eof = true
+			}
+			return nil
+		}
+
+		for !eof {
+			err := listSome()
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		slices.SortFunc(ret, func(a, b proto.GitURL) int {
+			return strings.Compare(string(a.Repo), string(b.Repo))
+		})
+
+		return ret, nil
+
+	}
+
+	var ret []proto.GitURL
+	ret, err = listForParty(arg.Cfg.ActingAs)
 	if err != nil {
 		return nil, err
 	}
 
-	listSome := func() error {
-		opts := rem.KVListOpts{
-			Num:   128,
-			Start: strt,
-		}
-
-		ls, err := tm.List(m, cfg, path, did, opts)
-		if err != nil {
-			return err
-		}
-		for _, ent := range ls.Ents {
-			if !ent.Value.IsDir() {
-				continue
-			}
-			ret = append(ret, proto.GitURL{
-				Proto: proto.GitProtoType_Foks,
-				Fqp:   *fqpp,
-				Repo:  proto.GitRepo(ent.Name),
-			})
-		}
-		if ls.Nxt != nil {
-			did = &ls.Nxt.Id
-			strt = ls.Nxt.Nxt
-		} else {
-			eof = true
-		}
-		return nil
+	if !arg.AllTeams {
+		return ret, nil
+	}
+	teamMinder, err := m.G().TeamMinder()
+	if err != nil {
+		return nil, err
 	}
 
-	for !eof {
-		err := listSome()
+	tmp, err := teamMinder.ListMemberships(m.MetaContext)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, t := range tmp.Teams {
+		var fqtp proto.FQTeamParsed
+		tm := t.Team.Fqp.FQTeam()
+		if tm == nil {
+			continue
+		}
+		fqtp.Team = proto.NewParsedTeamWithFalse(tm.Team)
+		host := proto.NewParsedHostnameWithFalse(tm.Host)
+		fqtp.Host = &host
+		repos, err := listForParty(&fqtp)
 		if err != nil {
 			return nil, err
 		}
+		ret = append(ret, repos...)
 	}
-
-	slices.SortFunc(ret, func(a, b proto.GitURL) int {
-		return strings.Compare(string(a.Repo), string(b.Repo))
-	})
 
 	return ret, nil
 }
