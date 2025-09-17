foks-proj/go-foks

commit 9d99188f8a9eb76bf70b5a0b4a85306dcf088cdc

cli: simplify args to `key switch` and `key remove` (#209)
- no more query-by-key-genus or role
- provide an FQU, which works if not ambiguous
- if ambiguous, you can also provide a keyID
- no ability to just use a KeyID though it should work, so maybe we add this late
- close #208
- released in v0.1.3

client/foks/cmd/key.go
@@ -10,7 +10,6 @@ import (
 	"github.com/foks-proj/go-foks/client/foks/cmd/common_ui"
 	"github.com/foks-proj/go-foks/client/foks/cmd/ui"
 	"github.com/foks-proj/go-foks/client/libclient"
-	"github.com/foks-proj/go-foks/lib/core"
 	"github.com/foks-proj/go-foks/lib/libterm"
 	"github.com/foks-proj/go-foks/proto/lcl"
 	proto "github.com/foks-proj/go-foks/proto/lib"
@@ -18,24 +17,12 @@ import (
 )
 
 type switchCmdCfg struct {
-	fqu      string
-	role     string
-	yubi     bool
-	device   bool
-	backup   bool
-	botToken bool
-	keyID    string
+	fqu   string
+	keyID string
 }
 
-func (s *switchCmdCfg) flagCommand(c *cobra.Command, verb string, devFlag bool) {
+func (s *switchCmdCfg) flagCommand(c *cobra.Command, verb string) {
 	c.Flags().StringVarP(&s.fqu, "user", "u", "", "fully qualified user (via ID or name)")
-	c.Flags().StringVar(&s.role, "role", "", "role (e.g., 'o', 'a', or 'm0' or 'm-30')")
-	c.Flags().BoolVar(&s.yubi, "yubi", false, fmt.Sprintf("%s yubikey", verb))
-	if devFlag {
-		c.Flags().BoolVar(&s.device, "device", false, fmt.Sprintf("%s device", verb))
-	}
-	c.Flags().BoolVar(&s.backup, "backup", false, fmt.Sprintf("%s backup key", verb))
-	c.Flags().BoolVar(&s.botToken, "bot-token", false, fmt.Sprintf("%s bot token", verb))
 	c.Flags().StringVar(&s.keyID, "key-id", "", fmt.Sprintf("key ID to %s", verb))
 }
 
@@ -50,7 +37,7 @@ func switchCmd(m libclient.MetaContext) *cobra.Command {
 			return runSwitch(m, cmd, &cfg, arg)
 		},
 	}
-	cfg.flagCommand(cmd, "switch to", true)
+	cfg.flagCommand(cmd, "switch to")
 	return cmd
 }
 
@@ -73,7 +60,7 @@ interactively prompt you to select a key to remove.`, 0),
 			return runRemove(m, cmd, &cfg, args)
 		},
 	}
-	cfg.flagCommand(cmd, "remove", false)
+	cfg.flagCommand(cmd, "remove")
 	return cmd
 }
 
@@ -315,41 +302,10 @@ func (s *switchCmdCfg) parse() (*lcl.LocalUserIndexParsed, error) {
 	if err != nil {
 		return nil, err
 	}
-	if fqu == nil {
-		if len(s.role) > 0 {
-			return nil, ArgsError("can only use -r flag with -u flag")
-		}
-		if eid != nil {
-			return nil, ArgsError("can only use --key-id flag with -u flag")
-		}
-		return nil, nil
-	}
-
-	role, err := parseRole(s.role, &proto.OwnerRole)
-	if err != nil {
-		return nil, err
-	}
-	var kg *proto.KeyGenus
-	switch {
-	case s.yubi:
-		tmp := proto.KeyGenus_Yubi
-		kg = &tmp
-	case s.device:
-		tmp := proto.KeyGenus_Device
-		kg = &tmp
-	case s.backup:
-		tmp := proto.KeyGenus_Backup
-		kg = &tmp
-	case s.botToken:
-		tmp := proto.KeyGenus_BotToken
-		kg = &tmp
-	}
 
 	ret := lcl.LocalUserIndexParsed{
-		Fqu:      *fqu,
-		Role:     *role,
-		KeyGenus: kg,
-		KeyID:    eid,
+		Fqu:   *fqu,
+		KeyID: eid,
 	}
 	return &ret, nil
 }
@@ -366,9 +322,6 @@ func runRemove(m libclient.MetaContext, cmd *cobra.Command, cfg *switchCmdCfg, a
 	}
 	if swarg == nil {
 		return ui.RunRemove(m, cli)
-	}
-	if swarg.KeyGenus != nil && *swarg.KeyGenus == proto.KeyGenus_Device {
-		return core.BadArgsError("cannot remove a device key; use `foks key revoke` instead")
 	}
 	return cli.RemoveKey(m.Ctx(), *swarg)
 }

client/foks/cmd/parse.go
@@ -20,13 +20,6 @@ func (s BadSubCommandError) Error() string {
 	return "bad subcommand: " + string(s)
 }
 
-func parseRole(s string, def *proto.Role) (*proto.Role, error) {
-	if len(s) == 0 {
-		return def, nil
-	}
-	return proto.RoleString(s).Parse()
-}
-
 func parseFqu(s string) (*proto.FQUserParsed, error) {
 	if len(s) == 0 {
 		return nil, nil

client/libclient/all_users.go
@@ -491,13 +491,6 @@ func LookupUserInAllUsers(
 		if !ok {
 			return false, nil
 		}
-		ok, err = u.Role.Eq(i.Role)
-		if err != nil {
-			return false, err
-		}
-		if !ok {
-			return false, nil
-		}
 		return true, nil
 	}
 
@@ -513,9 +506,6 @@ func LookupUserInAllUsers(
 			continue
 		}
 
-		if u.KeyGenus != nil && e.KeyGenus != *u.KeyGenus {
-			continue
-		}
 		if u.KeyID != nil && !e.Key.Eq(u.KeyID) {
 			continue
 		}

integration-tests/cli/revoke_test.go
@@ -277,7 +277,9 @@ func TestSelfProvisionThenRevoke(t *testing.T) {
 	require.True(t, findDevice())
 
 	aInfoDev := getActiveUser(t, a)
-	a.runCmd(t, nil, "key", "switch", "--yubi", "-u", aStr)
+	yubiDev, err := aInfoYubi.Key.StringErr()
+	require.NoError(t, err)
+	a.runCmd(t, nil, "key", "switch", "-u", aStr, "--key-id", yubiDev)
 
 	dev, err := aInfoDev.Key.StringErr()
 	require.NoError(t, err)

proto-src/lcl/user.snowp
@@ -7,8 +7,8 @@ go:import "github.com/foks-proj/go-foks/proto/lib" as lib;
 
 struct LocalUserIndexParsed {
     fqu @0 : lib.FQUserParsed;
-    role @1 : lib.Role;
-    keyGenus @2 : Option(lib.KeyGenus);
+    // role @1 : lib.Role; // Deprecated 
+    // keyGenus @2 : Option(lib.KeyGenus); // Deprecated
     keyID @3 : lib.EntityID; // optional, for disambiguation
 }
 

proto-src/lib/common.snowp
@@ -347,7 +347,7 @@ struct PartyName {
 enum PartyType {
     User @0;
     Team @1;
-gte.Desc.RepoName}
+}
 
 variant ParsedParty switch (s : bool) {
     case false @0 : PartyID;

proto/lcl/user.go
@@ -13,17 +13,15 @@ import (
 import lib "github.com/foks-proj/go-foks/proto/lib"
 
 type LocalUserIndexParsed struct {
-	Fqu      lib.FQUserParsed
-	Role     lib.Role
-	KeyGenus *lib.KeyGenus
-	KeyID    lib.EntityID
+	Fqu   lib.FQUserParsed
+	KeyID lib.EntityID
 }
 type LocalUserIndexParsedInternal__ struct {
-	_struct  struct{} `codec:",toarray"` //lint:ignore U1000 msgpack internal field
-	Fqu      *lib.FQUserParsedInternal__
-	Role     *lib.RoleInternal__
-	KeyGenus *lib.KeyGenusInternal__
-	KeyID    *lib.EntityIDInternal__
+	_struct     struct{} `codec:",toarray"` //lint:ignore U1000 msgpack internal field
+	Fqu         *lib.FQUserParsedInternal__
+	Deprecated1 *struct{}
+	Deprecated2 *struct{}
+	KeyID       *lib.EntityIDInternal__
 }
 
 func (l LocalUserIndexParsedInternal__) Import() LocalUserIndexParsed {
@@ -34,24 +32,6 @@ func (l LocalUserIndexParsedInternal__) Import() LocalUserIndexParsed {
 			}
 			return x.Import()
 		})(l.Fqu),
-		Role: (func(x *lib.RoleInternal__) (ret lib.Role) {
-			if x == nil {
-				return ret
-			}
-			return x.Import()
-		})(l.Role),
-		KeyGenus: (func(x *lib.KeyGenusInternal__) *lib.KeyGenus {
-			if x == nil {
-				return nil
-			}
-			tmp := (func(x *lib.KeyGenusInternal__) (ret lib.KeyGenus) {
-				if x == nil {
-					return ret
-				}
-				return x.Import()
-			})(x)
-			return &tmp
-		})(l.KeyGenus),
 		KeyID: (func(x *lib.EntityIDInternal__) (ret lib.EntityID) {
 			if x == nil {
 				return ret
@@ -62,14 +42,7 @@ func (l LocalUserIndexParsedInternal__) Import() LocalUserIndexParsed {
 }
 func (l LocalUserIndexParsed) Export() *LocalUserIndexParsedInternal__ {
 	return &LocalUserIndexParsedInternal__{
-		Fqu:  l.Fqu.Export(),
-		Role: l.Role.Export(),
-		KeyGenus: (func(x *lib.KeyGenus) *lib.KeyGenusInternal__ {
-			if x == nil {
-				return nil
-			}
-			return (*x).Export()
-		})(l.KeyGenus),
+		Fqu:   l.Fqu.Export(),
 		KeyID: l.KeyID.Export(),
 	}
 }

commit 345a4d94a95111d31b5c3d55f039b5c793b1acb3

cli: fix crash in `key switch` and `key remove`
- most of the time, --user is not specified, but with this bug, the CLI front-end would crash
- bug introduced in #209
- close #210
- released in v0.1.4 (emergency fix)

client/foks/cmd/key.go
@@ -302,6 +302,13 @@ func (s *switchCmdCfg) parse() (*lcl.LocalUserIndexParsed, error) {
 	if err != nil {
 		return nil, err
 	}
+	if fqu == nil && len(eid) > 0 {
+		return nil, ArgsError("user must be specified if key-id is provided")
+	}
+
+	if fqu == nil {
+		return nil, nil
+	}
 
 	ret := lcl.LocalUserIndexParsed{
 		Fqu:   *fqu,