5e3eb5b50df6bb779539b5b986cc8e32d5121f02
Maxwell Krohn <themax@gmail.com>
2026-03-27 10:37:14 -0600

    Bump to v0.1.7 for release

--- lib/core/version.go
+++ lib/core/version.go
@@ -18,7 +18,7 @@ const CurrentCompatibilityVersion proto.CompatibilityVersion = 1
 var CurrentSoftwareVersion = proto.SemVer{
 	Major: 0,
 	Minor: 1,
-	Patch: 6,
+	Patch: 7,
 }
 
 type ParsedSemVer struct {
